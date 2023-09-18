dcd3575a3d3fa938a6789269ec2868d315302df8
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2023-09-18 15:15:29 -0400

    Added code from L8

--- Django/website_demo/blog/models.py
+++ Django/website_demo/blog/models.py
@@ -3,7 +3,33 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth.hashers import make_password, check_password
 import re
 
+
+def validate_username(username):
+    if len(username) < 4:
+        raise ValidationError("Username shall be at least 4 characters")
+    if not username.isalnum():
+        raise ValidationError("Username shall be alphanumeric")
+
+def validate_pwd(pwd):
+    if len(pwd) < 8 or len(pwd) > 128:
+        raise ValidationError("password shall be at least 8 and not more than 128 chars")
+    if not any( [c.isupper() for c in pwd]):
+        raise ValidationError("Password shall have at least 1 uppercase")
+    if not any( [c.islower() for c in pwd]):
+        raise ValidationError("Password shall have at least 1 uppercase")
+    if not re.search("[&@!]", pwd):
+        raise ValidationError("pass does not have at least 1 special symbol")
+
+
 # TODO: User class
+class User(models.Model):
+    username = models.CharField(max_length=200, unique=True, validators=[validate_username])
+    password = models.TextField(validators=[validate_pwd])
+    email = models.EmailField(unique=True)
 
 
 # TODO: Post class
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=400)
+    content = models.TextField()
--- Django/website_demo/blog/views.py
+++ Django/website_demo/blog/views.py
@@ -6,16 +6,43 @@ from django.urls import reverse
 from django.core.exceptions import ValidationError
 from django.contrib.auth.hashers import make_password, check_password
 
+from .models import User
+
 def index(request):
     context = {'course': "CSE-60770", 'semester': 'FA23'}
     return render(request, 'blog/index.html', context)
 
 
 def register(request):
-    pass
+    if request.POST:
+        username = request.POST["username"]
+        pwd = request.POST["password"]
+        email  = request.POST["email"]
+        user = User(username=username, password=pwd, email=email)
+        try:
+            user.full_clean()
+            # all good
+            user.password = make_password(pwd)
+            user.save()
+            return HttpResponseRedirect("/blog/login")
+        except ValidationError as e:
+            return render(request, 'blog/register.html', {'errors': e})
+
+    return render(request, 'blog/register.html')
+
+
 
 def login(request):
-    pass
+    if request.POST:
+        username = request.POST["username"]
+        pwd = request.POST["password"]
+        user = User.objects.filter(username=username) # list
+        if user:
+            pass
+
+
+
+    return render(request, 'blog/login.html')
 
 
 def logout(request):
