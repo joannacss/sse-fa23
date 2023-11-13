a11bbf48df5f953bd18b96c4331cf7734f606d29
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2023-11-13 14:58:30 -0500

    demo 1

--- DynamicAnalyzer/demo1.py
+++ DynamicAnalyzer/demo1.py
@@ -3,23 +3,29 @@ import sys
 
 def analyze(frame, event, arg):
 	# get function code
+	function_code = frame.f_code
 
 	# get function name
+	function_name = function_code.co_name
 
 	# get line number
+	lineno = frame.f_lineno
 
 	# prints them f"{event} --> {function_name}:{lineno}"
+	print(f"{event} --> {function_name}:{lineno}")
 
 	# returns the function itself to track the new scope
+	return analyze
     
     
 
 def main():
 	# start tracing the invocation
-
+	sys.settrace(analyze)
 	output = triangle(2, 2, 1)
 
 	# stop tracing the invocation
+	sys.settrace(None)
 
 
 	print("Output at main()", output)
